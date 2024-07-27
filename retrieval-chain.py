f62996047760368c4221a571dcae351db802529f
pramodC135 <pramodmadhushan4@gmail.com>
2024-07-27 23:05:50 +0530

    modification

--- retrieval-chain.py
+++ retrieval-chain.py
@@ -2,46 +2,45 @@ from dotenv import load_dotenv
 load_dotenv()
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.documents import Document
-#from langchain.chains.combine_documents import create_stuff_documents_chain
-#from langchain_community.document_loaders import WebBaseLoader
+#from langchain_core.documents import Document
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.document_loaders import WebBaseLoader
 
 model = ChatOpenAI(
     model="gpt-3.5-turbo",
     temperature=0.7
 )
 
-docA = Document(
-    page_content="Generative AI is a type of artificial intelligence technology that can produce various types of content, including text, imagery, audio and synthetic data. The recent buzz around generative AI has been driven by the simplicity of new user interfaces for creating high-quality text, graphics and videos in a matter of seconds."
-)
+#docA = Document(
+#    page_content="Generative AI is a type of artificial intelligence technology that can produce various types of content, including text, imagery, audio and synthetic data. The recent buzz around generative AI has been driven by the simplicity of new user interfaces for creating high-quality text, graphics and videos in a matter of seconds."
+#)
 
-#def get_document_from_web(url):
-#    loader = WebBaseLoader(url)
-#    docs = loader.load()
-#    return docs
+def get_document_from_web(url):
+    loader = WebBaseLoader(url)
+    docs = loader.load()
+    return docs
 
 #print(get_document_from_web('https://docs.smith.langchain.com/user_guide'))
-
-
+docs = get_document_from_web('https://docs.smith.langchain.com/user_guide')
 
 #Create a prompt template
 prompt = ChatPromptTemplate.from_template(""""
 Answer the user's question :
-Contex: {contex}
+Contex: {context}
 Question : {input}
 """)
 
 #create LLM chain
-chain = prompt | model
-#chain = create_stuff_documents_chain(
-#    llm=model,
-#    prompt=prompt
-#)
+#chain = prompt | model
+chain = create_stuff_documents_chain(
+    llm=model,
+    prompt=prompt
+)
 
 response = chain.invoke({
-    "input": "What is Generative AI",
-    "contex" : [docA],
-    #"contex" : []
+    "input": "What is Generative AI",\
+    #"context" : [docA]
+    "contex" : docs
 })
 
-print(response)
+#print(response)
